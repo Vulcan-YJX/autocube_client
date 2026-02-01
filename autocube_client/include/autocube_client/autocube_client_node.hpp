67765f5be01e8bdb7c0199bbaab3a71fb1985aac
vulcan <jinxu.yu@directdrivetech.com>
2026-02-01 15:12:49 +0800

    feat: init prj.
    Signed-off-by: vulcan <jinxu.yu@directdrivetech.com>

---
+++ autocube_client/include/autocube_client/autocube_client_node.hpp
@@ -0,0 +1,54 @@
+// Copyright 2026 Vulcan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef AUTOCUBE_CLIENT__AUTOCUBE_CLIENT_NODE_HPP_
+#define AUTOCUBE_CLIENT__AUTOCUBE_CLIENT_NODE_HPP_
+
+#include <grpcpp/grpcpp.h>
+#include <twist.grpc.pb.h>
+
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+class AutocubeClientNode : public rclcpp::Node
+{
+public:
+  explicit AutocubeClientNode(const rclcpp::NodeOptions & options);
+
+  ~AutocubeClientNode();
+
+private:
+  void reader_twist_loop();
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
+
+  std::shared_ptr<grpc::Channel> channel_ = nullptr;
+  std::unique_ptr<autocube::TwistService::Stub> stub_ = nullptr;
+  grpc::ClientContext twist_context_;
+  std::shared_ptr<grpc::ClientReaderWriter<autocube::TwistMessage, autocube::TwistMessage>>
+    twist_stream_ = nullptr;
+
+  std::string address_;
+  std::string cmd_vel_topic_;
+
+  // Thread
+  std::thread reader_thread_;
+  std::atomic<bool> running_;
+};
+
+#endif  // AUTOCUBE_CLIENT__AUTOCUBE_CLIENT_NODE_HPP_
---
+++ autocube_client/src/autocube_client_node.cpp
@@ -0,0 +1,75 @@
+// Copyright 2026 Vulcan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "autocube_client/autocube_client_node.hpp"
+
+AutocubeClientNode::AutocubeClientNode(const rclcpp::NodeOptions & options)
+: Node("autocube_client_node", options), running_(true)
+{
+  this->get_parameter("address", address_);
+  this->get_parameter("cmd_vel_topic", cmd_vel_topic_);
+
+  twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(cmd_vel_topic_, 10);
+
+  channel_ = grpc::CreateChannel(address_, grpc::InsecureChannelCredentials());
+  stub_ = autocube::TwistService::NewStub(channel_);
+
+  twist_stream_ = stub_->TwistStream(&twist_context_);
+
+  if (!twist_stream_) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to create gRPC stream");
+    return;
+  }
+
+  reader_thread_ = std::thread(&AutocubeClientNode::reader_twist_loop, this);
+
+  RCLCPP_INFO(this->get_logger(), "gRPC Server listening on: '%s'", address_.c_str());
+}
+
+void AutocubeClientNode::reader_twist_loop()
+{
+  autocube::TwistMessage msg;
+
+  while (running_ && twist_stream_->Read(&msg)) {
+    geometry_msgs::msg::Twist ros_msg;
+    ros_msg.linear.x = msg.linear_x();
+    ros_msg.linear.y = msg.linear_y();
+    ros_msg.linear.z = msg.linear_z();
+    ros_msg.angular.x = msg.angular_x();
+    ros_msg.angular.y = msg.angular_y();
+    ros_msg.angular.z = msg.angular_z();
+
+    twist_pub_->publish(ros_msg);
+  }
+
+  RCLCPP_WARN(this->get_logger(), "Reader twist thread exited");
+}
+
+AutocubeClientNode::~AutocubeClientNode()
+{
+  running_ = false;
+
+  if (twist_stream_) {
+    twist_stream_->WritesDone();
+  }
+
+  if (reader_thread_.joinable()) {
+    reader_thread_.join();
+  }
+
+  auto status = twist_stream_->Finish();
+  if (!status.ok()) {
+    RCLCPP_ERROR(this->get_logger(), "gRPC finish error: %s", status.error_message().c_str());
+  }
+}
