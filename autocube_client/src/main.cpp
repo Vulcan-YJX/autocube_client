7127cffb8a407f546e53b0dcabad4f63a123573a
vulcan <jinxu.yu@directdrivetech.com>
2026-02-01 17:57:13 +0800

    feat: add native dns.
    Signed-off-by: vulcan <jinxu.yu@directdrivetech.com>

--- autocube_client/src/main.cpp
+++ autocube_client/src/main.cpp
@@ -12,10 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstdlib>  // for setenv / _putenv
+#include <iostream>
+
 #include "autocube_client/autocube_client_node.hpp"
 
 int main(int argc, char * argv[])
 {
+  #ifdef _WIN32
+    _putenv("GRPC_DNS_RESOLVER=native");  // Windows
+  #else
+      setenv("GRPC_DNS_RESOLVER", "native", 1); // Linux/macOS
+  #endif
   rclcpp::init(argc, argv);
 
   auto options = rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true);
