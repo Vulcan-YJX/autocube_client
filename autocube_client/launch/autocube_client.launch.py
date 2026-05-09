857ee69f09f4c9861b750db8c78623f24c308aad
vulcan <jinxu.yu@directdrivetech.com>
2026-05-09 16:56:35 +0800

    fix: fix node restart issue.
    Signed-off-by: vulcan <jinxu.yu@directdrivetech.com>

--- autocube_client/launch/autocube_client.launch.py
+++ autocube_client/launch/autocube_client.launch.py
@@ -1,9 +1,12 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, EmitEvent
+from launch.event_handlers import OnProcessExit
+from launch.events import Shutdown
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     config = os.path.join(
@@ -22,20 +25,30 @@ def generate_launch_description():
         )
     )
 
+    autocube_node = Node(
+        package='autocube_client',
+        executable='autocube_client_node',
+        name='autocube_client_node',
+        output='screen',
+        parameters=[
+            config,
+            # {
+            #     "battery1_topic": "/" + d1_namespace + "/system_status_broadcaster/battery1",
+            #     "battery2_topic": "/" + d1_namespace + "/system_status_broadcaster/battery2",
+            #     "cmd_vel_topic": "/" + d1_namespace + "/user_command/cmd_twist"
+            # }
+        ]
+    )
+
+    shutdown_on_node_exit = RegisterEventHandler(
+        OnProcessExit(
+            target_action=autocube_node,
+            on_exit=[EmitEvent(event=Shutdown(reason='autocube_client_node exited'))]
+        )
+    )
+
     return LaunchDescription([
         ranger_launch,
-        Node(
-            package='autocube_client',
-            executable='autocube_client_node',
-            name='autocube_client_node',
-            output='screen',
-            parameters=[
-                config,
-                # {
-                #     "battery1_topic": "/" + d1_namespace + "/system_status_broadcaster/battery1",
-                #     "battery2_topic": "/" + d1_namespace + "/system_status_broadcaster/battery2",
-                #     "cmd_vel_topic": "/" + d1_namespace + "/user_command/cmd_twist"
-                # }
-            ]
-        )
+        autocube_node,
+        shutdown_on_node_exit,
     ])
